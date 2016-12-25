6705ce1e2c47b7dc24017b266a3287f33c440aff
KB09 <triaptahadiryorda@yahoo.co.id>
2016-12-25 10:37:50 +0000

    add bootscript and abs path for django

--- IVMNM/bootku.sh
+++ IVMNM/bootku.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 echo " ---- Begin IVMNM ----- "
 echo " ---- Starting INIT SEQUENCE ----"
-/bin/mount -t proc none /proc
-/bin/mount -t sysfs none /sys
 /etc/init.d/mountkernfs.sh start
 /etc/init.d/mountall.sh start
 /etc/init.d/fake-hwclock start
 /etc/init.d/hostname.sh start
 /etc/init.d/udev start
+/bin/mount -t proc none /proc
+/bin/mount -t sysfs none /sys
 /etc/init.d/procps start
 /etc/init.d/networking start # turn on network interfaces
 # /etc/init.d/dhclient start
 /etc/init.d/hostapd start
-# /etc/init.d/isc-dhcp-server start
+#/etc/init.d/isc-dhcp-server start
 /sbin/sulogin
 
-echo " --- SETUP NETWORK LOGIN --- "
+echo " --- START AUTORUN --- "
 /home/pi/autorun.sh
--- autorun.sh
+++ autorun.sh
@@ -2,4 +2,4 @@
 
 sudo service apache2 stop &
 sudo /usr/sbin/hostapd /etc/hostapd/hostapd.conf &
-sudo python3 manage.py runserver 0.0.0.0:80
+sudo python3 /home/pi/IVMNM/manage.py runserver 0.0.0.0:80
