f0286e9d53b3655f9fbf4c150e173f39491899d8
dzulham.azham <dzulham.azham@gmail.com>
2016-12-25 12:13:22 +0700

    Added GPIO LED

--- dashboard/views.py
+++ dashboard/views.py
@@ -16,9 +16,9 @@ def index(req) :
 		######################################################
 		#GPIO PIN CONFIGURATION SORTED ASC FROM LEFT TO RIGHT
 		GPIO_PIN_BCM	= [7, 11, 12, 13, 15, 16, 18, 22, 29, 31, 32, 33, 35, 36, 37, 38, 40]
-		GPIO_PIN_BOARD	= [4, 17, 18, 27, 22, 23, 24, 25, 5, 6, 12, 13, 19, 16, 26, 20, 21]
+		GPIO_PIN_BOARD	= [4, 17, 18, 27, 22, 23, 24, 25, 05, 06, 12, 13, 19, 16, 26, 20, 21]
 		######################################################
-		api_rpi_gpio(GPIO_PIN_BOARD[int(req.POST['gpio_pin'])], True)
+		api_rpi_gpio(GPIO_PIN_BOARD[int(req.POST['gpio_pin'])])
 
 		user = User.objects.get(pk=req.POST['user_id'])
 		meal = Meal.objects.get(pk=req.POST['meal_id'])
@@ -44,10 +44,21 @@ def logout(req) :
 	auth.logout(req)
 	return redirect('/login')
 
-def api_rpi_gpio(pin, mode_board):
-	GPIO.setmode(GPIO.BOARD if mode_board else GPIO.BCM)
+def api_rpi_gpio(pin):
+	GPIO.setmode(GPIO.BOARD)
+	#Setup pin for dynamo, GPIO5 for LED Green, GPIO6 for LED Red
+	led_green_pin = GPIO_PIN_BOARD[8]
+	led_red_pin = GPIO_PIN_BOARD[9]
 	GPIO.setup(pin, GPIO.OUT)
+	GPIO.setup(led_green_pin, GPIO.OUT)
+	GPIO.setup(led_red_pin, GPIO.OUT)
+
+	GPIO.output(led_green_pin, GPIO.LOW)
+	GPIO.output(led_red_pin, GPIO.HIGH)
 	GPIO.output(pin, GPIO.HIGH)
 	time.sleep(1)
+	GPIO.output(led_red_pin, GPIO.LOW)
+	GPIO.output(led_green_pin, GPIO.HIGH)
 	GPIO.output(pin, GPIO.LOW)
+
 	GPIO.cleanup()
