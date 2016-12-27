e39bd7f5f94fea687359436e06f3cf9c884f5a63
KB09 <triaptahadiryorda@yahoo.co.id>
2016-12-26 21:13:08 -0500

    update bootku dan autorun.sh

--- autorun.sh
+++ autorun.sh
@@ -1,25 +1,25 @@
 #!/bin/sh
 
 sudo service apache2 stop
-WLAN_STATS=$(sudo /etc/init.d/dhcpcd status | grep "Active: active")
-echo "W: _$WLAN_STATS_"
+#WLAN_STATS=$(sudo /etc/init.d/dhcpcd status | grep "Active: active")
+#echo "W: _$WLAN_STATS_"
 #while [ -z "$WLAN_STATS" ]; do
 #	echo "waiting for dhcpcd setup..."
 #	sleep 1
 #	WLAN_STATS=$(sudo /etc/init.d/dhcpcd status | grep "Active: active");	
 #done
 
-WLAN_STATS=$(sudo /etc/init.d/hostapd status | grep "Active: active")
-echo "W: _$WLAN_STATS_"
+#WLAN_STATS=$(sudo /etc/init.d/hostapd status | grep "Active: active")
+#echo "W: _$WLAN_STATS_"
 #while [ -z "$WLAN_STATS" ]; do
 #	echo "waiting for hostapd setup..."
 #	sleep 1
 #	WLAN_STATS=$(sudo /etc/init.d/hostapd status | grep "Active: active");	
 #done
 
-sudo /etc/init.d/networking status
-sudo /etc/init.d/dhcpcd status
-sudo /etc/init.d/hostapd status
+#sudo /etc/init.d/networking status
+#sudo /etc/init.d/dhcpcd status
+#sudo /etc/init.d/hostapd status
 
 sudo /usr/sbin/hostapd /etc/hostapd/hostapd.conf &
 sleep 2
--- bootku.sh
+++ bootku.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 echo " ---- Begin IVMNM ----- "
 echo " ---- Starting INIT SEQUENCE ----"
+#/bin/mount --rw /dev/mmcblk0p2 /
+#/bin/mount -t vfat /dev/mmcblk0p1 /boot
 /bin/mount -t proc none /proc
 /bin/mount -t sysfs none /sys
 /etc/init.d/mountkernfs.sh start
 /etc/init.d/mountall.sh start
+#/etc/init.d/mountfs.sh start
+#/etc/init.d/mountdevsubfs.sh start
+
 /etc/init.d/fake-hwclock start
 /etc/init.d/hostname.sh start
 /etc/init.d/udev start
@@ -13,9 +18,10 @@ echo " ---- Starting INIT SEQUENCE ----"
 # /etc/init.d/dh client start
 /etc/init.d/hostapd start
 # /etc/init.d/isc-dhcp-server start
-# /sbin/sulogin
+#/sbin/sulogin
 # /etc/init.d/dhcpcd start
-/etc/init.d/dnsmasq start
+sudo /etc/init.d/dnsmasq start
+# /bin/login
 
 echo " --- SETUP NETWORK LOGIN --- "
 
